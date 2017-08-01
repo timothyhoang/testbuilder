0a740ee92d1c1a90c4c59cb872cfc298eeda46c7
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-08-01 16:13:47 -0700

    Modified detectNetwork in detectNetwork.js to add Visa and MasterCard detection
    Refactored code to remove if-then-else statement in detectNetwork and added heler functions is___ where ___ is a card in supported networks (Diner's Club, American Express, Visa, MasterCard).

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,19 +7,44 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-  cardNumber = cardNumber.toString();
+var isDinersClub = function(cardNumber) {
   var prefix = cardNumber.slice(0, 2);
-  if ((prefix === '38' || prefix === '39') && cardNumber.length === 14) {
+  if (['38', '39'].includes(prefix) && cardNumber.length === 14) {
     return 'Diner\'s Club';
-  } else if ((prefix === '34' || prefix === '37') && cardNumber.length === 15) {
+  }
+};
+
+var isAmericanExpress = function(cardNumber) {
+  var prefix = cardNumber.slice(0, 2);
+  if (['34', '37'].includes(prefix) && cardNumber.length === 15) {
     return 'American Express';
   }
 };
 
+var isVisa = function(cardNumber) {
+  var prefix = cardNumber[0];
+  if (prefix === '4' && [13, 16, 19].includes(cardNumber.length)) {
+    return 'Visa';
+  }
+};
 
+var isMasterCard = function(cardNumber) {
+  var prefix = cardNumber.slice(0, 2);
+  if (['51', '52', '53', '54', '55'].includes(prefix) && cardNumber.length === 16) {
+    return 'MasterCard';
+  }
+};
+
+var detectNetwork = function(cardNumber) {
+  // Note: `cardNumber` will always be a string
+  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+
+  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  return isDinersClub(cardNumber) ||
+         isAmericanExpress(cardNumber) ||
+         isVisa(cardNumber) ||
+         isMasterCard(cardNumber);
+};
