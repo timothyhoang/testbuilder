2a564e33ffd6d5026ba31253e3cf9cfc54adc572
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-08-01 16:32:16 -0700

    Modified detectNetwork.test.js to allow first three it tests to pass

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -18,7 +18,6 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // You will not be able to proceed with a failing test. 
 
   it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
   });
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
@@ -33,7 +32,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // A test should only fail if the expected behavior doesn't match the actual.
   it('Throws an error when expected behavior does not match actual behavior', function() {
     var even = function(num){
-      return num/2 === 0;
+      return num % 2 === 0;
     }
 
     if(even(10) !== true) {
